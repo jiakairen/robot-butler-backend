88daa2ea637f96171344a1ade0f8ad37ee06778c
Jiakai Ren <jiakai.ren@gmail.com>
2022-12-08 12:39:45 +1100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'pages/home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20221208012634_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.text :first_name
+      t.text :last_name
+      t.text :email
+      t.text :topics
+      t.text :frequency
+
+      t.timestamps
+    end
+  end
+end
